8de1a35e383aaef87026ce833c205c7d0c692838
yuliatinagustina <143861230+yuliatinagustina@users.noreply.github.com>
2024-10-16 18:09:59 +0700

    Add files via upload

---
+++ UTS/mavenproject1/src/main/java/sistemhewanpeliharaan/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sistemhewanpeliharaan.mavenproject1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
